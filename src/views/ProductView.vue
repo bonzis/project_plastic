7f8c5f0cf59689e0c49ea61877856fe7be0781e2
lellesessuale <riccobonoga@gmail.com>
2023-05-10 22:59:42 +0200

    img remuve for

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -4,7 +4,7 @@
         <v-container   class="product-container" >
             
             <v-row align="center" >
-                <v-col v-for="ciaos in ciao"  align="center"   >
+                <v-col  align="center"   >
                     <Prodotto  title="Logo" authors="Il logo del progetto!" imgUrl="../../public/img/logo.png" />
                     <Prodotto  title="Infografica" authors="Una breve infografica che racconta il mondo della sostenibilità \n, mantenendo il focus sullo spreco della plastica" imgUrl="../../public/img/1294117.png" />
                     <Prodotto  title="Fumetto" authors="Un fumetto che sfrutta un contesto divertente per fungere da denuncia per quanto riguarda lo spreco della plastica" imgUrl="../../public/tappi.jpg" />
